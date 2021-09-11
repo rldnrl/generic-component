21db89f0149a9fd48a9a7b37bebac1bf44e4e6e1
enginecode-cpu <bassmatics1002@gmail.com>
2021-09-11 20:51:09 +0900

    first commit

---
+++ src/components/List.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+type Props<TItem> = {
+  items: TItem[]
+  renderEmpty: () => React.ReactElement
+  renderItem: (item: TItem) => React.ReactElement
+}
+
+/**
+ * 
+ * @param items
+ * @param renderEmpty
+ * @param renderItem
+ * `name`이 기본으로 들어가는 객체를 담는다.
+ * 
+ * ex)
+ * ```ts
+ * const items = [
+ *   {
+ *     name: "Cpp",
+ *     age: 23,
+ *   },
+ *   {
+ *     name: "Python",
+ *     age: 45,
+ *   },
+ *   {
+ *     name: "Java",
+ *     age: 40,
+ *   },
+ *   ...
+ * ]
+ * ```
+ * @returns ReactElement
+ */
+
+function List<TItem extends { name: string }>({ items, renderEmpty, renderItem }: Props<TItem>): React.ReactElement {
+  if (items.length === 0) return renderEmpty()
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default List
